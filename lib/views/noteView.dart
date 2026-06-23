44b301829519f07859553a200273d9eb1bf2cd04
ubong <ubonggeorge126@gmail.com>
2026-06-23 22:56:46 +0100

    delete function

--- lib/views/noteView.dart
+++ lib/views/noteView.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:reindeer/views/Home.dart';
 import '../controller/note_controller.dart';
+import 'package:provider/provider.dart';
 import '../Models/note_model.dart';
 
 class Noteview extends StatelessWidget {
@@ -13,10 +14,34 @@ class Noteview extends StatelessWidget {
   Widget build(BuildContext context){
 return Scaffold(
 appBar: AppBar(title: Text( note.title),),
-body: Padding(padding: EdgeInsets.all(16.0),
+body: Padding(padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
 child: Column(
+  crossAxisAlignment: CrossAxisAlignment.start,
   children: [
-    Text(note.content)
+    Text(note.content),
+
+    Spacer(),
+    Row(
+      children: [
+        Text(
+          note.createdAt.toString().substring(10, 19)
+          ),
+          Spacer(),
+
+          IconButton(
+            onPressed: (){
+            context.read<NotesViewModel>().deleteNote(note.id);
+
+            Navigator.pop(context); //go back
+          }, 
+          icon: Icon(Icons.delete, color: Colors.redAccent,))
+
+          
+      ],
+    ),
+    SizedBox(
+            height: 18.0,
+          ),
   ],
 ),),
 );
